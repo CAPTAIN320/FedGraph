58a68d80b8c8c4249c818f176cb530d3f03f4ded
Yaqub Mahmoud <yaqub320@gmail.com>
2023-06-25 19:53:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -21,20 +21,14 @@ recorder = {'train_loss': {'clients': [[] for k in range(args.split)], 'server':
 g = data_load(args)
 e = g.edges(form='eid')
 graphs = data_split(g, args)
-print(graphs)
 # subfigs(graphs,args)
 # FL initialize
 server = Server(g, args)
 clients = [Client(k, graphs[k], args) for k in range(args.num_clients)]
-sybils = [Sybil(k, graphs[-1], args) for k in range(args.num_sybils)]
 for i in clients:
     i.g.remove_edges(i.g.edges(form='eid'))
     # i.g = add_self_loop(i.g)
     add_edges(i.g)
-for s in sybils:
-    s.g.remove_edges(s.g.edges(form='eid'))
-    add_edges(s.g)
-
 new_client = Client(-1, graphs[-1], args)
 # FLearning
 for _ in range(int(args.n_epochs)):
@@ -49,43 +43,27 @@ for _ in range(int(args.n_epochs)):
         # # Evaluate
         # acc = evaluate(clients[k].model, clients[k], mask='test')
         # recorder['test_acc']['clients'][k+1].append(acc)
-    for s in sybils:
-        s.fork(server)
-        s.local_update()
-
     # Merge
-    all_clients = clients + sybils  # Combine regular clients and sybil clients
-    server.merge(all_clients)
-
+    server.merge(clients)
     acc = evaluate(server.model, server)
     recorder['test_acc']['server'].append(acc)
     acc = evaluate(server.model, new_client)
     recorder['test_acc']['clients'][0].append(acc)
-
 # Evaluate
 for k in range(len(clients)):
     acc = evaluate(clients[k].model, clients[k], mask='test')
     print("Client{}: {:.2%}".format(clients[k].id, acc))
-
-for s in sybils:
-    acc = evaluate(s.model, s, mask='test')
-    print("Sybil{}: {:.2%}".format(s.id, acc))
-
 acc = evaluate(server.model, server)
 print("Server: {:.2%}".format(acc))
-
 server.g.remove_edges(server.g.edges(form='eid'))
 server.g = add_self_loop(server.g)
 acc = evaluate(server.model, server)
 print("Server: {:.2%}".format(acc))
-
 acc = evaluate(server.model, new_client)
 print("Client{}: {:.2%}".format(new_client.id, acc))
-
 new_client.g.remove_edges(new_client.g.edges(form='eid'))
 new_client.g = add_self_loop(new_client.g)
 acc = evaluate(server.model, new_client)
 print("Client{}: {:.2%}".format(new_client.id, acc))
-
 torch.save(recorder, './saves/recorder.pt')
 plotfig(recorder, args)
