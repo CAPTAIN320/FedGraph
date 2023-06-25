b1194ed7157ff92c86e1cd279f4ca57a79527b1c
Yaqub Mahmoud <yaqub320@gmail.com>
2023-06-25 19:52:15 +0900

    Update fl.py

--- fl.py
+++ fl.py
@@ -25,27 +25,6 @@ class Client(object):
         for E in range(self.args.E):
             train(self)
 
-class Sybil(object):
-    def __init__(self, id, graph, args):
-        self.args = args
-        self.id = id + 1
-        self.g = graph.int().to(args.device) ## adjacency matrix is here
-        self.feats = self.g.ndata['feat']
-        self.labels = self.g.ndata['label']
-        self.train_mask = self.g.ndata['train_mask']
-        self.val_mask = self.g.ndata['val_mask']
-        self.test_mask = self.g.ndata['test_mask']
-        self.model = init_model(self.args).to(args.device)
-        self.optimizer = init_optimizer(self.model, self.args)
-
-    def fork(self, server):
-        self.model = copy.deepcopy(server.model)
-        self.optimizer = init_optimizer(self.model, self.args)
-
-    def local_update(self):
-        for E in range(self.args.E):
-            train(self)
-
 class Server(object):
     def __init__(self, graph, args):
         self.args = args
@@ -67,3 +46,24 @@ class Server(object):
             for i in range(len(all_clients)):
                 self.dict[key] += clients_states[i][key]
             self.dict[key] /= len(all_clients)
+
+# class Sybil(object):
+#     def __init__(self, id, graph, args):
+#         self.args = args
+#         self.id = id + 1
+#         self.g = graph.int().to(args.device) ## adjacency matrix is here
+#         self.feats = self.g.ndata['feat']
+#         self.labels = self.g.ndata['label']
+#         self.train_mask = self.g.ndata['train_mask']
+#         self.val_mask = self.g.ndata['val_mask']
+#         self.test_mask = self.g.ndata['test_mask']
+#         self.model = init_model(self.args).to(args.device)
+#         self.optimizer = init_optimizer(self.model, self.args)
+
+#     def fork(self, server):
+#         self.model = copy.deepcopy(server.model)
+#         self.optimizer = init_optimizer(self.model, self.args)
+
+#     def local_update(self):
+#         for E in range(self.args.E):
+#             train(self)
