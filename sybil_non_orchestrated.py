991c1858e009e2b7271227873bc573f15d2728d2
Yaqub Mahmoud <yaqub320@gmail.com>
2023-07-03 22:43:25 +0900

    Update sybil_non_orchestrated.py

--- sybil_non_orchestrated.py
+++ sybil_non_orchestrated.py
@@ -4,6 +4,7 @@ from data import *
 from sybil_utils import data_split
 
 import torch
+from random import randint
 
 data = CoraGraphDataset()
 g = data[0]
@@ -30,40 +31,41 @@ def modify_g_edges(graph):
 
     return sybil_graph
 
-def sybil_add_nodes(graph, amount=0):
-    graph.add_nodes(amount)
-    return graph
+# def sybil_add_nodes(graph, amount=0):
+#     graph.add_nodes(amount)
+#     return graph
 
-def sybil_add_edges(graph, src_node_id, dest_node_id, no_of_new_edges=0, target_edge_feat='', new_edge_value=0): # CPT-NOTE: Node is added if src & dest nodes do NOT exist
+def add_sybil_edges(
+                graph,
+                src_node_id, # Should be less than no. of nodes
+                dest_node_id, # Should be less than no. of nodes
+                target_edge_feat, # Should be a valid edge feature name
+                new_edge_value,
+                no_of_new_edges=0,
+            ): # CPT-NOTE: Node is added if src & dest nodes do NOT exist
     for i in range(no_of_new_edges):
         graph.add_edges(src_node_id, dest_node_id, {target_edge_feat: torch.tensor([new_edge_value])})
     return graph
 
-# sybil_graph = add_g_edges(chosen_graph)
-# print("num of sybil nodes: ",sybil_graph.num_nodes())
-# print("num of sybil edges: ",sybil_graph.num_edges())
+# print('List of Edge Indexes: ', chosen_graph.edata['_ID'].tolist())
 
-# print(sybil_graph.edata)
+# sybil_graph = chosen_graph
 
-# sybil_graph = sybil_add_nodes(sybil_graph, 10)
+# no_of_new_edges = 5
+# source_node = sybil_graph.num_nodes() - 1
+# destination_node = sybil_graph.num_nodes() - 2
+# target_edge_feature = '_ID'
+# new_edge_value = randint(0,10000)
+
+# sybil_graph = add_edges(
+#                             sybil_graph,
+#                             source_node, # ShouldLess than the amount of nodes in the graph
+#                             destination_node,
+#                             target_edge_feature,
+#                             new_edge_value,
+#                             no_of_new_edges,
+#                         )
+
 # print("new num of sybil nodes: ",sybil_graph.num_nodes())
 # print("new num of sybil edges: ",sybil_graph.num_edges())
-
-sybil_graph = chosen_graph
-
-# TODO: Understand more
-sybil_graph = sybil_add_edges(sybil_graph, sybil_graph.num_nodes() - 1, sybil_graph.num_nodes() - 2)
-
-print('List of Edge Indexes: ', chosen_graph.edata['_ID'].tolist())
-
-no_of_new_edges = 5
-source_node = sybil_graph.num_nodes() - 1
-destination_node = sybil_graph.num_nodes() - 2
-target_edge_feature = '_ID'
-new_edge_value = 9
-
-# sybil_graph = sybil_add_edges(sybil_graph, source_node, destination_node, no_of_new_edges)
-sybil_graph = sybil_add_edges(sybil_graph, torch.tensor(5), torch.tensor([5,1]), no_of_new_edges, target_edge_feature, new_edge_value)
-print("new num of sybil nodes: ",sybil_graph.num_nodes())
-print("new num of sybil edges: ",sybil_graph.num_edges())
-print('List of Sybil Edge Indexes: ', sybil_graph.edata['_ID'].tolist())
+# print('List of Sybil Edge Indexes: ', sybil_graph.edata['_ID'].tolist())
