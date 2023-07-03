674f9da75225c63073eedf77c5a5c261176404ea
Yaqub Mahmoud <yaqub320@gmail.com>
2023-07-03 21:09:58 +0900

    Update sybil_non_orchestrated.py

--- sybil_non_orchestrated.py
+++ sybil_non_orchestrated.py
@@ -19,7 +19,7 @@ print("num of nodes: ",chosen_graph.num_nodes())
 print("num of edges: ",chosen_graph.num_edges())
 print("is homogenous: ",chosen_graph.is_homogeneous)
 
-def add_g_edges(graph):
+def modify_g_edges(graph):
     # Clone the graph
     sybil_graph = graph.clone()
 
@@ -34,23 +34,33 @@ def sybil_add_nodes(graph, amount=0):
     graph.add_nodes(amount)
     return graph
 
-def sybil_add_edges(graph, src_node_id, dest_node_id): # CPT-NOTE: Node is added if src & dest nodes do NOT exist
-    graph.add_edges(src_node_id, dest_node_id)
+def sybil_add_edges(graph, src_node_id, dest_node_id, no_of_new_edges=0): # CPT-NOTE: Node is added if src & dest nodes do NOT exist
+    for i in range(no_of_new_edges):
+        graph.add_edges(src_node_id, dest_node_id)
     return graph
 
-sybil_graph = add_g_edges(chosen_graph)
-print("num of sybil nodes: ",sybil_graph.num_nodes())
-print("num of sybil edges: ",sybil_graph.num_edges())
+# sybil_graph = add_g_edges(chosen_graph)
+# print("num of sybil nodes: ",sybil_graph.num_nodes())
+# print("num of sybil edges: ",sybil_graph.num_edges())
 
-print(sybil_graph.edata)
+# print(sybil_graph.edata)
 
 # sybil_graph = sybil_add_nodes(sybil_graph, 10)
 # print("new num of sybil nodes: ",sybil_graph.num_nodes())
 # print("new num of sybil edges: ",sybil_graph.num_edges())
 
+sybil_graph = chosen_graph
+
 # TODO: Understand more
-sybil_graph = sybil_add_edges(sybil_graph, 0, 1)
+sybil_graph = sybil_add_edges(sybil_graph, sybil_graph.num_nodes() - 1, sybil_graph.num_nodes() - 2)
+
+print('List of Edge Indexes: ', chosen_graph.edata['_ID'].tolist())
+
+no_of_new_edges = 5
+source_node = sybil_graph.num_nodes() - 1
+destination_node = sybil_graph.num_nodes() - 2
+
+sybil_graph = sybil_add_edges(sybil_graph, source_node, destination_node, no_of_new_edges)
 print("new num of sybil nodes: ",sybil_graph.num_nodes())
 print("new num of sybil edges: ",sybil_graph.num_edges())
-
-print(sybil_graph.edata)
+print('List of Sybil Edge Indexes: ', sybil_graph.edata['_ID'].tolist())
