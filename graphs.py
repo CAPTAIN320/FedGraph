8ce0fdc8052b9e797349d04b3b53c39cc076278c
Yaqub Mahmoud <yaqub320@gmail.com>
2023-07-10 21:01:55 +0900

    update

--- graphs.py
+++ graphs.py
@@ -1,13 +1,34 @@
 import os
 import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 
+
+### ATTACK 1
+#1 plot_accuracy_over_epoch for varying percentage of sybils
+data = pd.read_csv('results/A1_citeseer_0_sybils.csv')
+num_sybils = 0
+epochs = data['Epoch']
+accuracy = data['Accuracy']
+plt.plot(epochs, accuracy)
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.title(f'Accuracy over Epochs {num_sybils}% Sybils')
+plt.show()
+
+
+
+
+
+
+
+plt.plot(epochs, accuracy)
 model_accuracy = [0, 10, 25, 47, 76]
 percentage_of_sybils = [0, 10, 20, 30, 40, 50]
 num_of_epoch = [1, 5, 100, 500, 1000]
 num_of_fake_edges = [0, 1, 10, 100, 1000, 10000]
 
-os.makedirs('results', exist_ok=True)
+os.makedirs('graphs', exist_ok=True)
 
 ### ATTACK 1
 #1 plot_accuracy_per_epoch for varying percentage of sybils
--- main.py
+++ main.py
@@ -51,7 +51,7 @@ total_clients = len(clients)
 num_sybil_clients = args.num_sybils
 sybil_clients = random.sample(range(total_clients), num_sybil_clients)
 
-attack_type = 'orchestratedfdf'
+attack_type = 'orchestrated'
 num_fake_edges = 10
 
 for index, client in enumerate(clients):
@@ -115,7 +115,7 @@ recorder['test_acc']['test_client'] = test_client_acc
 save_accuracy_csv(recorder, f'./results/A1_{args.dataset}_{args.num_sybils}_sybils.csv', args)
 
 # Attack 2
-save_accuracy_csv(recorder, f'./results/A2_{args.dataset}_{args.num_sybils}_sybils_{num_fake_edges}_same-values.csv', args)
+# save_accuracy_csv(recorder, f'./results/A2_{args.dataset}_{args.num_sybils}_sybils_{num_fake_edges}_same-values.csv', args)
 # CPT-NOTE: Add to the file name of Attack 2 file depending on what you are doing e.g. random-values, same-values,
 
 # Evaluate Clients
