84e6582f65e6513ca9c1c077c7384fd7adc4dee9
Yaqub Mahmoud <yaqub320@gmail.com>
2023-07-16 16:32:28 +0900

    Update graphs_2.py

--- graphs_2.py
+++ graphs_2.py
@@ -2,14 +2,20 @@ import os
 import matplotlib.pyplot as plt
 import pandas as pd
 
-os.makedirs('graphs', exist_ok=True)
+os.makedirs('graphs_2', exist_ok=True)
 
 datasets = ['citeseer']
-num_sybils_array = [10, 20, 30, 40, 50]
 
 plt.figure()  # Create a new figure
 
+##### ATTACK 1 ###########
+num_sybils_array = [0, 10, 20, 30, 40, 50]
+
+
+
+##### ATTACK 2 ###########
 # A2_citeseer_accuracy_num-of-fake-edges
+num_sybils_array = [10, 20, 30, 40, 50]
 for num_sybils in num_sybils_array:
     data = pd.read_csv(f'results_2/A2_citeseer_{num_sybils}-sybils_fake-edges.csv')
     num_fake_edges = data['num_fake_edges']
@@ -21,7 +27,7 @@ plt.ylabel('Accuracy')
 plt.title('Accuracy vs Number of Fake Edges')
 plt.grid(True)
 plt.legend()  # Add legend based on the labels in the plot
-plt.savefig('./graphs/A2_citeseer_accuracy_num-of-fake-edges.png')
+plt.savefig('./graphs_2/A2_citeseer_accuracy_num-of-fake-edges.png')
 plt.close()
 
 # A2_citeseer_random-values
@@ -34,7 +40,7 @@ plt.ylabel('Accuracy')
 plt.title('Accuracy vs Sybil % random edge values')
 plt.grid(True)
 plt.ylim(0)
-plt.savefig('./graphs/A2_citeseer_random-values.png')
+plt.savefig('./graphs_2/A2_citeseer_random-values.png')
 plt.close()
 
 # A2_citeseer_same-values
@@ -47,5 +53,5 @@ plt.ylabel('Accuracy')
 plt.title('Accuracy vs Sybil % same edge values')
 plt.grid(True)
 plt.ylim(0)
-plt.savefig('./graphs/A2_citeseer_same-values.png')
+plt.savefig('./graphs_2/A2_citeseer_same-values.png')
 plt.close()
