64a69d5e28fe6644d95b2e93f326707a0ea575e9
Yaqub Mahmoud <yaqub320@gmail.com>
2023-07-16 16:00:02 +0900

    Update graphs_2.py

--- graphs_2.py
+++ graphs_2.py
@@ -13,7 +13,8 @@ for num_sybils in num_sybils_array:
     data = pd.read_csv(f'results_2/A2_citeseer_{num_sybils}-sybils_fake-edges.csv')
     num_fake_edges = data['num_fake_edges']
     accuracy = data['Accuracy']
-    plt.plot(num_fake_edges, accuracy, marker='o', linestyle='-', label=f'{num_sybils} sybils')
+    label = f'{num_sybils}% sybils'
+    plt.plot(num_fake_edges, accuracy, marker='o', linestyle='-', label=label)
 
 plt.xlabel('Number of Fake Edges')
 plt.ylabel('Accuracy')
@@ -23,4 +24,18 @@ plt.grid(True)
 plt.legend()  # Add legend based on the labels in the plot
 
 plt.savefig('./graphs/A2_citeseer_accuracy_num-of-fake-edges.png')
+plt.close()
+
+
+
+
+data = pd.read_csv(f'results_2/A2_citeseer_random-values.csv')
+num_sybils = data['Sybil %']
+accuracy = data['Accuracy']
+plt.plot(num_sybils, accuracy, marker='o', linestyle='-', color='b')
+plt.xlabel('Sybil %')
+plt.ylabel('Accuracy')
+plt.title('Accuracy vs Sybil %')
+plt.grid(True)
+plt.savefig('./graphs/A2_citeseer_random-values.png')
 plt.close()
