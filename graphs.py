08e8d1fdb496058a78cf6b554ccde501acefe35e
Yaqub Mahmoud <yaqub320@gmail.com>
2023-07-16 12:25:14 +0900

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -60,32 +60,32 @@ for index, dataset in enumerate(datasets):
 
 
 
-### ATTACK 1
-#1 plot_accuracy_over_epoch for varying percentage of sybils
-for index, dataset in enumerate(datasets):
-    for num_sybils in num_sybils_array:
-        name = f'A1_{dataset}_{num_sybils}_sybils'
-        data = pd.read_csv(f'results/{name}.csv')
-        epochs = data['Epoch']
-        accuracy = data['Accuracy']
-        plt.plot(epochs, accuracy)
-        plt.xlabel('Epoch')
-        plt.ylabel('Accuracy')
-    plt.title(f'Accuracy over Epochs {num_sybils}% Sybils')
-    plt.savefig(f'./graphs/{name}.png')
-    plt.close()
+# ### ATTACK 1
+# #1 plot_accuracy_over_epoch for varying percentage of sybils
+# for index, dataset in enumerate(datasets):
+#     for num_sybils in num_sybils_array:
+#         name = f'A1_{dataset}_{num_sybils}_sybils'
+#         data = pd.read_csv(f'results/{name}.csv')
+#         epochs = data['Epoch']
+#         accuracy = data['Accuracy']
+#         plt.plot(epochs, accuracy)
+#         plt.xlabel('Epoch')
+#         plt.ylabel('Accuracy')
+#     plt.title(f'Accuracy over Epochs {num_sybils}% Sybils')
+#     plt.savefig(f'./graphs/{name}.png')
+#     plt.close()
 
 
-#2 plot_accuracy_per_sybil_percentage at 10 epochs
-for index, dataset in enumerate(datasets):
-    for num_sybils in num_sybils_array:
-        name = f'A2_{dataset}_{num_sybils}_sybils_{num_fake_edges}_same-values'
-        data = pd.read_csv(f'results/{name}.csv')
-        sybils = data['Sybil %']
-        accuracy = data['Accuracy']
-        plt.plot(sybils, accuracy)
-        plt.xlabel('Sybil %')
-        plt.ylabel('Accuracy')
-    plt.title(f'Accuracy over Sybil % {num_sybils}% Sybils')
-    plt.savefig(f'./graphs/{name}.png')
-    plt.close()
+# #2 plot_accuracy_per_sybil_percentage at 10 epochs
+# for index, dataset in enumerate(datasets):
+#     for num_sybils in num_sybils_array:
+#         name = f'A2_{dataset}_{num_sybils}_sybils_{num_fake_edges}_same-values'
+#         data = pd.read_csv(f'results/{name}.csv')
+#         sybils = data['Sybil %']
+#         accuracy = data['Accuracy']
+#         plt.plot(sybils, accuracy)
+#         plt.xlabel('Sybil %')
+#         plt.ylabel('Accuracy')
+#     plt.title(f'Accuracy over Sybil % {num_sybils}% Sybils')
+#     plt.savefig(f'./graphs/{name}.png')
+#     plt.close()
